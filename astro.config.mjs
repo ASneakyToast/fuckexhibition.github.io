a43ab22c3886c216ff08bdb8b6f63a521c977d44
asneakytoast <joelnotlithgow@gmail.com>
2023-04-12 17:23:36 -0700

    Changed dist directory for github pages deploy.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: "https://fuck-exhibition.com",
+
+  /* Only use when deploying other ways */
   // base: "/fuckexhibition.github.io",
-  outDir: "../dist",
+  // outDir: "../dist",
 });
